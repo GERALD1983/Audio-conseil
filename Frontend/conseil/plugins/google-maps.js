8eef5c4399e7a91671ae5e7bc0f0903c06940c46
Gerald-Ferron <67054420+GERALD1983@users.noreply.github.com>
2021-03-24 10:12:02 +0100

    Update google-maps.js

--- Frontend/conseil/plugins/google-maps.js
+++ Frontend/conseil/plugins/google-maps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyDMBKWdAOYDDFNxLq7YOwJK6XHSLJ3XRC0",
+    key: "secret",
     libraries: "places" // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
     // OR: libraries: 'places,drawing,visualization'
